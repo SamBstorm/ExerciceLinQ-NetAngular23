79269b2f304ae862fa286ed0e7826b5a93ce7c71
Samuel Legrain <samuel.legrain@bstorm.be>
2023-03-28 17:03:10 +0200

    Add correction Module 3

--- ExerciceLinQ/Program.cs
+++ ExerciceLinQ/Program.cs
@@ -62,3 +62,57 @@ foreach (string item in result_2_3)
 {
     Console.WriteLine(item);
 }*/
+
+/*3.1*/
+/*Pour chaque étudiant né avant 1955, donner le nom, le résultat annuel et le statut.
+Le statut prend la valeur « OK » si l’étudiant à obtenu au moins 12 comme résultat annuel
+et « KO » dans le cas contraire.*/
+
+/*var result_3_1 = dc.Students.Where(s => s.BirthDate.Year < 1955)
+                            .Select(s => new
+                            {
+                                Nom = s.Last_Name,
+                                Resultat = s.Year_Result,
+                                Status = (s.Year_Result >= 12) ? "Ok" : "KO"
+                            });
+var result_3_1 = from Student s in dc.Students
+                 where s.BirthDate.Year < 1955
+                 select new {
+                     Nom = s.Last_Name,
+                     Resultat = s.Year_Result,
+                     Status = (s.Year_Result >= 12) ? "Ok" : "KO"
+                 };
+
+foreach (var item in result_3_1)
+{
+    Console.WriteLine($"{item.Nom} {item.Resultat} : {item.Status}");
+}*/
+
+/*3.2*/
+/*Donner pour chaque étudiant entre 1955 et 1965 le nom, le résultat annuel et la
+catégorie à laquelle il appartient. La catégorie est fonction du résultat annuel obtenu ; un
+résultat inférieur à 10 appartient à la catégorie « inférieure », un résultat égal à 10 appartient
+à la catégorie « neutre », un résultat autre appartient à la catégorie « supérieure ».*/
+
+/*var result_3_2 = dc.Students.Where(s => s.BirthDate.Year >= 1955)
+                            .Where(s => s.BirthDate.Year <= 1965)
+                            //.Where(s => s.BirthDate.Year >= 1955 && s.BirthDate.Year <= 1965)
+                            .Select(s => new
+                            {
+                                s.Last_Name,
+                                s.Year_Result,
+                                Category = s.Year_Result > 10 ? "Supérieur" : s.Year_Result == 10 ? "Neutre" : "Inférieur"
+                            });
+
+var result_3_2 = from Student s in dc.Students
+                 where s.BirthDate.Year >= 1955 && s.BirthDate.Year <= 1965
+                 select new {
+                                s.Last_Name,
+                                s.Year_Result,
+                                Category = s.Year_Result > 10 ? "Supérieur" : s.Year_Result == 10 ? "Neutre" : "Inférieur"
+                            };
+
+foreach (var item in result_3_2)
+{
+    Console.WriteLine($"{item.Last_Name} {item.Year_Result} : {item.Category}");
+}*/
